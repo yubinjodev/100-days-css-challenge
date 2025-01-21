828a7d4881f4200f4a6625736b8ff97be2590536
yubinjodev <yubinjodev@gmail.com>
2025-01-21 13:33:25 +0900

    Chore: Upload day 2 files

---
+++ day2/javascript/main.js
@@ -0,0 +1,42 @@
+let open = false;
+
+const handleClickOpen = () => {
+  $(".hamburger-menu__bar")
+    .css({
+      position: "absolute",
+      transform: "translate(-50%, -50%)",
+    })
+    .animate(
+      {
+        top: "50%",
+        left: "50%",
+      },
+      5000, // Duration in milliseconds (5 seconds)
+      () => {}
+    );
+};
+
+const handleClickClose = () => {
+  $(".hamburger-menu__bar")
+    .css({
+      position: "unset",
+      transform: "unset",
+    })
+    .animate(
+      {
+        top: "unset",
+        left: "unset",
+      },
+      5000 // Duration in milliseconds (5 seconds)
+    );
+};
+
+$(".hamburger-menu").click(() => {
+  if (open) {
+    open = false;
+    handleClickClose();
+  } else {
+    open = true;
+    handleClickOpen();
+  }
+});
