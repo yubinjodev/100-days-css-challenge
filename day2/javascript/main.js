8dda0cbeb4317c5685d7ded51dfd4a20e9e3363f
yubinjodev <yubinjodev@gmail.com>
2025-01-23 01:13:15 +0900

    Feat: Make tertiary bar fade out on click

--- day2/javascript/main.js
+++ day2/javascript/main.js
@@ -7,51 +7,12 @@ const handleClickClose = () => {
     })
     .fadeOut()
 
-  $(".hamburger__bar").animate({
-    marginTop: "0px",
-    marginBottom: "0px",
-  })
-
-  $(".hamburger__bar--primary").css({
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "100px",
-  })
-  $(".hamburger__bar--secondary").css({
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "100px",
-  })
-
   isOpen = false
 }
 
 const handleClickOpen = () => {
   $(".hamburger__bar--tertiary").fadeIn().animate({
     width: "100%",
-  })
-
-  $(".hamburger__bar").animate({
-    marginTop: "0px",
-    marginBottom: "0px",
-  })
-
-  $(".hamburger__bar--primary").css({
-    position: "unset",
-    top: "unset",
-    left: "unset",
-    transform: "unset",
-  })
-
-  $(".hamburger__bar--secondary").css({
-    position: "unset",
-    top: "unset",
-    left: "unset",
-    transform: "unset",
   })
 
   isOpen = true
