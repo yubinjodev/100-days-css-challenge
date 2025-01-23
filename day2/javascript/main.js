d7c155068c27ecfa005dbcd334cb2404a4f50447
yubinjodev <yubinjodev@gmail.com>
2025-01-23 13:19:53 +0900

    Feat: Make primary and secondary bars fade into center

--- day2/javascript/main.js
+++ day2/javascript/main.js
@@ -2,18 +2,56 @@ let isOpen = true
 
 const handleClickClose = () => {
   $(".hamburger__bar--tertiary")
-    .animate({
-      width: "30%",
-    })
+    .animate(
+      {
+        width: "30%",
+      },
+      500,
+    )
     .fadeOut()
+
+  $(".hamburger__bar--secondary")
+    .css({
+      transform: "translate(-50%, -50%)",
+    })
+    .animate({
+      top: "50%",
+    })
+
+  $(".hamburger__bar--primary")
+    .css({
+      transform: "translate(-50%, -50%)",
+    })
+    .animate({
+      top: "50%",
+    })
 
   isOpen = false
 }
 
 const handleClickOpen = () => {
-  $(".hamburger__bar--tertiary").fadeIn().animate({
-    width: "100%",
-  })
+  $(".hamburger__bar--tertiary").fadeIn().animate(
+    {
+      width: "100%",
+    },
+    500,
+  )
+
+  $(".hamburger__bar--secondary")
+    .animate({
+      top: "0px",
+    })
+    .css({
+      transform: "translate(-50%, 0%)",
+    })
+
+  $(".hamburger__bar--primary")
+    .animate({
+      top: "50px",
+    })
+    .css({
+      transform: "translate(-50%, 0%)",
+    })
 
   isOpen = true
 }
